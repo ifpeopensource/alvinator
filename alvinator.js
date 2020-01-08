26b774c14a893546d71b9d12a8cfff7f4fe4de47
Fábio Papais <56613389+fabiopapaiss@users.noreply.github.com>
2020-01-07 22:40:50 -0300

    Add files via upload

--- alvinator.js
+++ alvinator.js
@@ -1,5 +1,4 @@
 function pitag(a, b, c) {
-
     // a**2 = b**2 + c**2 --> The pythagoream theorem
     
     if (a == 0 && b > 0 && c > 0) {
@@ -8,8 +7,11 @@ function pitag(a, b, c) {
         if (aSolved > 0) {
             return aSolved
         }
-        if (aSolved == 0 || (b ** 2 + c ** 2) < 0) {
-            return `O resultado foi ${aSolved}, errado algo está!`
+        if (aSolved == 0) {
+            return `O resultado foi ${aSolved}! Jovem, isto está altamente errado!`
+        }
+        if ((b ** 2 + c ** 2) < 0) {
+            return `O resultado foi ${b ** 2 + c ** 2}! Jovem, isto está altamente errado!`
         }
     } if (a > 0 && b == 0 && c > 0) {
         var bSolved = Math.sqrt(a ** 2 - c ** 2)
@@ -17,17 +19,23 @@ function pitag(a, b, c) {
         if (bSolved > 0) {
             return bSolved
         }
-        if (bSolved == 0 || (a ** 2 - c ** 2) < 0) {
-            return `O resultado foi ${bSolved}, errado algo está!`
-        }
+        if (bSolved == 0) {
+            return `O resultado foi ${bSolved}! Jovem, isto está altamente errado!`
+        } 
+        if ((a ** 2 - c ** 2) < 0) {
+            return `O resultado foi ${a ** 2 - c ** 2}! Jovem, isto está altamente errado!`
+        }    
     } if (a > 0 && b > 0 && c == 0) {
         var cSolved = Math.sqrt(a ** 2 - b ** 2)
 
         if (cSolved > 0) {
             return cSolved
         }
-        if (cSolved == 0 || (a ** 2 - b ** 2) < 0) {
-            return `O resultado foi ${cSolved}, errado algo está!`
+        if (cSolved == 0) {
+            return `O resultado foi ${cSolved}, Jovem, isto está altamente errado!`
+        }
+        if ((a ** 2 - b ** 2) < 0) {
+            return `O resultado foi ${a ** 2 - b ** 2}, Jovem, isto está altamente errado!`
         }
     } if ((a == 0 && b == 0) || (b == 0 && c == 0) || (a == 0 && c == 0)) {
         return 'Para descobrir um terceiro valor do triângulo, devemos ter pelo menos 2 valores'
