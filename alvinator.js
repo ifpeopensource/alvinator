b064d01e4789bfb33843ca510cda5eaebe63cb57
Fábio Papais <56613389+fabiopapaiss@users.noreply.github.com>
2020-01-08 21:12:08 -0300

    Add files via upload

--- alvinator.js
+++ alvinator.js
@@ -1,31 +1,36 @@
+// All the functions works as equations that we use in the problem solving with right triangles
+// Probably (I can't predict the future), in most cases, each function represents one equation, but sometimes a function is just another form of that same equation
+// Let's go!
+
+// NORMAL PYTHAGORAS
 function pitag(a, b, c) {
     // a**2 = b**2 + c**2 --> The pythagoream theorem
     
-    if (a == 0 && b > 0 && c > 0) {
-        var aSolved = Math.sqrt(b ** 2 + c ** 2)
+    if (a == 0 && b > 0 && c > 0) { // Those first-level "if's" recognize the input of the user
+        var aSolved = Math.sqrt(b ** 2 + c ** 2) // Math for finding the value of this variable with pythagorean theorem
 
-        if (aSolved > 0) {
+        if (aSolved > 0) { // For positive values only (hehe it's geometry)
             return aSolved
         }
-        if (aSolved == 0) {
+        if (aSolved == 0) { // If the value is 0, that triangle doesn't exist
             return `O resultado foi ${aSolved}! Jovem, isto está altamente errado!`
         }
-        if ((b ** 2 + c ** 2) < 0) {
+        if ((b ** 2 + c ** 2) < 0) { // In case of the value being negative
             return `O resultado foi ${b ** 2 + c ** 2}! Jovem, isto está altamente errado!`
-        }
-    } if (a > 0 && b == 0 && c > 0) {
+        } // The subsequent functions work on the same way
+    } if (a > 0 && b == 0 && c > 0) { 
         var bSolved = Math.sqrt(a ** 2 - c ** 2)
 
         if (bSolved > 0) {
             return bSolved
-        }
+        } 
         if (bSolved == 0) {
             return `O resultado foi ${bSolved}! Jovem, isto está altamente errado!`
         } 
         if ((a ** 2 - c ** 2) < 0) {
             return `O resultado foi ${a ** 2 - c ** 2}! Jovem, isto está altamente errado!`
         }    
-    } if (a > 0 && b > 0 && c == 0) {
+    } if (a > 0 && b > 0 && c == 0) { 
         var cSolved = Math.sqrt(a ** 2 - b ** 2)
 
         if (cSolved > 0) {
@@ -37,8 +42,8 @@ function pitag(a, b, c) {
         if ((a ** 2 - b ** 2) < 0) {
             return `O resultado foi ${a ** 2 - b ** 2}, Jovem, isto está altamente errado!`
         }
-    } if ((a == 0 && b == 0) || (b == 0 && c == 0) || (a == 0 && c == 0)) {
-        return 'Para descobrir um terceiro valor do triângulo, devemos ter pelo menos 2 valores'
+    } if ((a == 0 && b == 0) || (b == 0 && c == 0) || (a == 0 && c == 0) || (a == 0 && b == 0 && c == 0)) { // This one identifies if it has just one input
+        return 'PENSE JOVEM! Dados insuficientes'
     }
 }
 console.log(pitag(2,0,0))
