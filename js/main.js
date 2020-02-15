c0691d42db5efb5c61d13ae17ad2437d56e23e16
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-02-14 22:45:02 -0300

    infoButtonArea added, but not styled

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 import decisions from './decisions.js'
-let 
+
+let infoButton = document.getElementById('infoButton')
+let bigContainer = document.getElementById('bigContainer')
+var body = document.getElementsByTagName("body")[0]
 
 function main() {
     decisions()
 }
 
+function infoButtonArea() {
+    var infoButtonArea = document.createElement("div")
+    body.appendChild(infoButtonArea)
+    infoButtonArea.setAttribute("id", "infoButtonArea")
+    console.log("test")
+    console.log("test")
+    //infoButtonArea.style.padding = "20px"
+    //infoButtonArea.style.backgroundColor = "white"
+    //TODO Styles and animations
+}
+
 inputCalculate.addEventListener("click", main)
+infoButton.addEventListener("click", infoButtonArea)
