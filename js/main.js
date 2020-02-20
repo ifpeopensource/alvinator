ea9a30ac7d7dc7e88ed2a668ff2f116a72de19a4
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-02-19 23:21:12 -0300

    info button

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 import decisions from './decisions.js'
 
-var infCount = 0
+
+infoButtonArea.setAttribute("id", "infoButtonArea")
+
+var i = 0
 
 let infoButton = document.getElementById('infoButton')
 let bigContainer = document.getElementById('bigContainer')
 var body = document.getElementsByTagName("body")[0]
 
-function infoButtonArea() {
-    infCount++
-    if (infCount%2 != 0) {
+function test() {
+    i++
+}
+
+function infoButtonAreaOn() {
+    if (i % 2 != 0) {
         var infoButtonArea = document.createElement("div")
         body.appendChild(infoButtonArea)
-        infoButtonArea.setAttribute("id", "infoButtonAreaOn")
-        console.log("test")    
-    }
-    else {
-        infoButtonArea.removeAttribute("id")
-        infoButtonArea.setAttribute("id", "infoButtonAreaOff")
+        console.log("true")
+    } if (i%2 == 0) {
+        document.getElementById("infoButtonArea").remove()
+        console.log("false")
     }
     //TODO Styles and animations
 }
-
 inputCalculate.addEventListener("click", decisions)
-infoButton.addEventListener("click", infoButtonArea)
-
+infoButton.addEventListener("click", infoButtonAreaOn, test)
