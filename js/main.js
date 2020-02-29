6fbdd72001b1722d69eb343bab0e985d6fe7b439
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-02-28 21:06:41 -0300

    infoButton logic done

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
 import decisions from './decisions.js'
 
-
-infoButtonArea.setAttribute("id", "infoButtonArea")
-
-var i = 0
-
-let infoButton = document.getElementById('infoButton')
 let bigContainer = document.getElementById('bigContainer')
 var body = document.getElementsByTagName("body")[0]
 
-function test() {
-    i++
-}
+let infoButtonAlternate = 0 
+let infoButton = document.getElementById('infoButton')
+let infoButtonArea = document.createElement("div") 
+body.appendChild(infoButtonArea)
 
-function infoButtonAreaOn() {
-    if (i % 2 != 0) {
-        var infoButtonArea = document.createElement("div")
-        body.appendChild(infoButtonArea)
-        console.log("true")
-    } if (i%2 == 0) {
-        document.getElementById("infoButtonArea").remove()
-        console.log("false")
+function infoButtonAreaDisplay() {
+    infoButtonAlternate++
+    if (infoButtonAlternate % 2 != 0) {
+        infoButtonArea.setAttribute("id", "infoButtonAreaOn")
+        console.log("infoButtonOn")
+    } if (infoButtonAlternate%2 == 0) {
+        infoButtonArea.setAttribute("id", "infoButtonAreaOff")
+        console.log("infoButtonOff")
     }
-    //TODO Styles and animations
 }
 inputCalculate.addEventListener("click", decisions)
-infoButton.addEventListener("click", infoButtonAreaOn, test)
+infoButton.addEventListener("click", infoButtonAreaDisplay)
